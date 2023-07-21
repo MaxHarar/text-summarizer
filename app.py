e9d72dabed7e05b8e7cc93c20781bb3a30d7963d
MaxHarar <77764626+MaxHarar@users.noreply.github.com>
2023-07-21 09:20:51 -0400

    finished

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import openai
 import os
 from text_summarizer.functions import summarize
 
+# Set your OpenAI API key
 openai.api_key = os.getenv('OPENAI_KEY')
 
 if "summary" not in st.session_state:
@@ -12,12 +13,20 @@ if "summary" not in st.session_state:
 
 st.title("Text Summarizer")
 
+# Dropdown for knowledge level options
+knowledge_level_options = ["beginner", "intermediate", "advanced", "expert", "specialist"]
+selected_knowledge_level = st.selectbox("Select your knowledge level:", knowledge_level_options)
+
 input_text = st.text_area(label="Enter full text:", value="", height=250)
 
 st.button(
     "Submit",
     on_click=summarize,
-    kwargs={"prompt": input_text},
+    kwargs={"prompt": input_text, "knowledge_level": selected_knowledge_level},  # Pass the selected knowledge level
 )
 
 output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
+
+
+
+""""""
--- text_summarizer/functions.py
+++ text_summarizer/functions.py
@@ -1,11 +1,26 @@
+
+
 import openai
 import streamlit as st
 
-def summarize(prompt):
-    augmented_prompt = f"summarize this text: {prompt}"
+def summarize(prompt, knowledge_level):
+    if knowledge_level == "beginner":
+        augmented_prompt = f"Summarize this text as if you were explaining it to someone with beginner-level knowledge: {prompt}"
+    elif knowledge_level == "intermediate":
+        augmented_prompt = f"Summarize this text as if you were explaining it to someone with intermediate-level knowledge: {prompt}"
+    elif knowledge_level == "advanced":
+        augmented_prompt = f"Summarize this text as if you were explaining it to someone with advanced-level knowledge: {prompt}"
+    elif knowledge_level == "expert":
+        augmented_prompt = f"Summarize this text as if you were explaining it to an expert: {prompt}"
+    elif knowledge_level == "specialist":
+        augmented_prompt = f"Summarize this text as if you were explaining it to a specialist: {prompt}"
+    else:
+        augmented_prompt = prompt
+
     st.session_state["summary"] = openai.Completion.create(
         model="text-davinci-003",
         prompt=augmented_prompt,
         temperature=.5,
         max_tokens=1000,
     )["choices"][0]["text"]
+
