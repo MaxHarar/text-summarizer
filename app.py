de5a94569df0f12aa885f03c9698a0129823b90f
MaxHarar <77764626+MaxHarar@users.noreply.github.com>
2023-07-27 10:04:46 -0400

    Tidy app UI comments

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ input_text = st.text_area(label="Enter full text:", value="", height=250)
 
 
 ##change
-# "Submit" button to generate summary at the selected knowledge level
+# "Submit" button  generate summary at the selected knowledge level
 if st.button(
     "Submit",
     on_click=summarize,
