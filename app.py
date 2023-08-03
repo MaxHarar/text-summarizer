038721864c949ca8499dedde4492027c766bbb9f
Max Harar <max.harar@gmail.com>
2023-08-03 10:34:03 -0400

    updates

--- app.py
+++ app.py
@@ -6,12 +6,10 @@ from text_summarizer.functions import summarize
 from dotenv import load_dotenv
 load_dotenv()
 
-
 openai.api_type = "azure"
 openai.api_version = "2023-05-15"
 openai.api_base = os.getenv("ENDPOINT")  # Your Azure OpenAI resource's endpoint value.
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 if "summary" not in st.session_state:
     st.session_state["summary"] = ""
@@ -26,9 +24,7 @@ selected_knowledge_level = st.selectbox("Select your knowledge level:", knowledg
 
 input_text = st.text_area(label="Enter full text:", value="", height=250)
 
-
-##change
-# "Submit" button  generate summary at the selected knowledge level
+# "Submit" button generates a summary at the selected knowledge level
 if st.button(
     "Submit",
     on_click=summarize,
@@ -37,3 +33,9 @@ if st.button(
     st.session_state["summary"] = st.session_state["summary"]
 
 output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
+
+# Note about the usage of AI and JNJ AI Guidelines
+st.write("Note: This tool use's Open AIs GPT 3.5 which may produce inaccurate information about people, places, "
+         "facts, or drug information. It is intended for internal use - response's should be validated. [JNJ AI "
+         "Guidelines]("
+         "https://home.jnj.com/sites/technology/news/1608773/embracing-the-power-of-generative-ai-responsibly)")
