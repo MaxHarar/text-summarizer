3b23c6d975bd5727af1af66d7583045d0fdf633d
MaxHarar <77764626+MaxHarar@users.noreply.github.com>
2023-08-22 15:53:41 -0400

    Inline API key for local testing

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import os
 from text_summarizer.functions import summarize
 
 # Set your OpenAI API key
-openai.api_key = os.getenv('OPENAI_KEY')
-
+openai.api_key = '***REMOVED***'
 if "summary" not in st.session_state:
     st.session_state["summary"] = ""
 if "compared_summary" not in st.session_state:
